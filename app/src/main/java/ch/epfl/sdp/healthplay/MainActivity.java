bbcf1463541a965d8ca6c91ab133d79bf0e47ec4
ozan-guven <ozan.guven@epfl.ch>
2022-03-03 21:11:54 +0100

    Create and add the basic Bootcamp app

---
+++ app/src/main/java/ch/epfl/sdp/healthplay/MainActivity.java
@@ -0,0 +1,28 @@
+package ch.epfl.sdp.healthplay;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "ch.epfl.sdp.healthplay.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, GreetingActivity.class);
+        EditText editText = findViewById(R.id.mainName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
