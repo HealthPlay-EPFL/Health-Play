e079db33b6f27104b3b350186c765fbc576cfd74
ozan-guven <ozan.guven@epfl.ch>
2022-03-07 14:28:38 +0100

    Create ProductInfoClient class

---
+++ app/src/main/java/ch/epfl/sdp/healthplay/model/ProductInfoClient.java
@@ -0,0 +1,10 @@
+package ch.epfl.sdp.healthplay.model;
+
+/**
+ * Class that represents the client used to get information
+ * of products given their bar codes.
+ *
+ * @author Ozan
+ */
+public class ProductInfoClient {
+}
