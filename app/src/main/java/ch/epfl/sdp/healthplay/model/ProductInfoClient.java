f6643556d8e766a58bdad9f13f8a248d23951cb7
ozan-guven <ozan.guven@epfl.ch>
2022-03-09 18:57:08 +0100

    Finish test cases for ProductInfoClient

--- app/src/main/java/ch/epfl/sdp/healthplay/model/ProductInfoClient.java
+++ app/src/main/java/ch/epfl/sdp/healthplay/model/ProductInfoClient.java
@@ -24,26 +24,36 @@ public final class ProductInfoClient {
     // Workaround to the fact that we cannot use Optional values..
     private final static String ERROR_STRING = "ERROR";
 
+    private final HttpURLConnection connection;
+
     // This is a class that just holds the method to get the product information,
     // there is no need to instantiate it.
-    private ProductInfoClient() {}
+    public ProductInfoClient(HttpURLConnection connection) {
+        if (connection == null) {
+            throw new IllegalArgumentException("Connection cannot be null.");
+        }
+        this.connection = connection;
+    }
+
+    public ProductInfoClient(String barcode) throws IOException {
+        if (barcode == null) {
+            throw new IllegalArgumentException("Barcode cannot be null.");
+        }
+        this.connection = (HttpURLConnection)
+                new URL(OPEN_FOOD_FACT_URL + barcode + ".json").openConnection();
+    }
 
     /**
      * When called with a given barcode, connects to the openfoodfact API and
      * asks for the information of the given product. The result is returned
      * in a String that is in JSON format.
      *
-     * @param barcode the barcode to get information from
      * @return the JSON string for that given product or "ERROR" if any error occurred
      */
-    public static String getInfo(String barcode) {
-        URL url;
-        HttpURLConnection connection = null;
+    public String getInfo() {
         StringBuilder response = new StringBuilder();
 
         try {
-            url = new URL(OPEN_FOOD_FACT_URL + barcode + ".json");
-            connection = (HttpURLConnection) url.openConnection();
             // Set a User-Agent in the HTTP header as asked by the openfoodfact website
             connection.setRequestProperty("User-Agent", USER_AGENT);
             connection.setRequestMethod("GET");
@@ -65,9 +75,7 @@ public final class ProductInfoClient {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
+            connection.disconnect();
         }
 
         return ERROR_STRING;
--- app/src/test/java/ch/epfl/sdp/healthplay/model/ProductInfoClientTest.java
+++ app/src/test/java/ch/epfl/sdp/healthplay/model/ProductInfoClientTest.java
@@ -1,12 +1,85 @@
 package ch.epfl.sdp.healthplay.model;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mock.*;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.exceptions.base.MockitoAssertionError;
+import org.mockito.junit.MockitoJUnit;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.*;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 public class ProductInfoClientTest {
+    private final static String TEST = "Test string";
+
+    AutoCloseable closeable;
+
+    ProductInfoClient client;
+
+    @Mock
+    HttpURLConnection connection;
+
+    @Before
+    public void setup() throws IOException {
+        closeable = MockitoAnnotations.openMocks(this);
+        when(connection.getResponseCode()).thenReturn(HttpURLConnection.HTTP_OK);
+        when(connection.getInputStream()).thenReturn(
+                new ByteArrayInputStream(TEST.getBytes(StandardCharsets.UTF_8)
+                )
+        );
+        doCallRealMethod().when(connection).setRequestMethod(anyString());
+        doCallRealMethod().when(connection).setRequestProperty(anyString(), anyString());
+    }
+
     @Test
-    public void test1() {
-        String p =  ProductInfoClient.getInfo("737628064502");
-        System.out.println(p);
-        System.out.println("asfasdfasdf");
+    public void testCorrectStringIsReturned() {
+        client = new ProductInfoClient(connection);
+        assertEquals("Test string", client.getInfo());
+    }
+
+    @Test
+    public void whenExceptionReturnErrorString() throws IOException {
+        when(connection.getInputStream()).thenThrow(IOException.class);
+
+        client = new ProductInfoClient(connection);
+        assertEquals("ERROR", client.getInfo());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullConnectionThrowsException() {
+        client = new ProductInfoClient((HttpURLConnection) null);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullBarcodeThrowsException() throws IOException {
+        client = new ProductInfoClient((String) null);
+    }
+
+    @After
+    public void finish() throws Exception {
+        closeable.close();
     }
 }
