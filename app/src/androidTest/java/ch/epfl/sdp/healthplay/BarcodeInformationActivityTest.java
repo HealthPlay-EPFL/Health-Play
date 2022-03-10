5a42451b58f18a5e7701167b98e61eb2171ab4e6
ozan-guven <ozan.guven@epfl.ch>
2022-03-10 14:14:21 +0100

    Add RunWith at the beggining of BarcodeInformationActivityTest

--- app/src/androidTest/java/ch/epfl/sdp/healthplay/BarcodeInformationActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/healthplay/BarcodeInformationActivityTest.java
@@ -9,9 +9,12 @@ import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(AndroidJUnit4.class)
 public class BarcodeInformationActivityTest {
     private static final String TEST_CODE = "737628064502";
 
@@ -20,7 +23,7 @@ public class BarcodeInformationActivityTest {
         Intent intent = new Intent(ApplicationProvider.getApplicationContext(), BarcodeInformationActivity.class);
         intent.putExtra(ProductInfoActivity.EXTRA_MESSAGE, TEST_CODE);
 
-        try (ActivityScenario<GreetingActivity> scenario = ActivityScenario.launch(intent)) {
+        try (ActivityScenario<GreetingActivity> ignored = ActivityScenario.launch(intent)) {
             Espresso.onView(withId(R.id.pName)).check(
                     ViewAssertions.matches(
                             ViewMatchers.withText("Thai peanut noodle kit includes stir-fry rice noodles & thai peanut seasoning")
