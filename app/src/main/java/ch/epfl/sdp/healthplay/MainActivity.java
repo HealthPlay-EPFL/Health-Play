fdda5bf5c303a317c298d146c861111d4e1ace86
Tetardfurieux <56368615+Tetardfurieux@users.noreply.github.com>
2022-03-11 10:44:16 +0100

    cleaned the mainActivity

--- app/src/main/java/ch/epfl/sdp/healthplay/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/healthplay/MainActivity.java
@@ -27,8 +27,4 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void goHome(View view) {
-        Intent intent = new Intent(this, HomeScreenActivity.class);
-        startActivity(intent);
-    }
 }
