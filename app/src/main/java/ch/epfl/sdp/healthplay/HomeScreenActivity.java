a76e820a50705f496130f4e3e0af301dd7a685f5
Tetardfurieux <56368615+Tetardfurieux@users.noreply.github.com>
2022-03-11 11:08:45 +0100

    done

--- app/src/main/java/ch/epfl/sdp/healthplay/HomeScreenActivity.java
+++ app/src/main/java/ch/epfl/sdp/healthplay/HomeScreenActivity.java
@@ -1,16 +1,11 @@
 package ch.epfl.sdp.healthplay;
 
-import static androidx.navigation.ViewKt.findNavController;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentContainerView;
-import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
-
-import android.content.Intent;
-import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -21,7 +16,7 @@ public class HomeScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        BottomNavigationView bottomNavigationView =(BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
         FragmentContainerView fragmentContainerView = (FragmentContainerView) findViewById(R.id.fragmentContainerView);
         //Set the navigation controller of the fragment container to the bottom navigation
         NavigationUI.setupWithNavController(bottomNavigationView, Navigation.findNavController(fragmentContainerView));
