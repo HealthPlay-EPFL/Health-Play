9432483865a4a20cce56da7c2fe71f03ab6e2809
Antonio06188 <antoine.metivier@epfl.ch>
2022-03-11 11:15:06 +0100

    add comment in WelcomeScreenActivity class

--- app/src/main/java/ch/epfl/sdp/healthplay/WelcomeScreenActivity.java
+++ app/src/main/java/ch/epfl/sdp/healthplay/WelcomeScreenActivity.java
@@ -20,6 +20,7 @@ public class WelcomeScreenActivity extends AppCompatActivity {
         ImageView logo = (ImageView) findViewById(R.id.logo_image);
         logo.setImageResource(R.drawable.logo);
         Intent intent = new Intent(this, MainActivity.class);
+        //Wait 3sec before launch HomeActivity
         handler.postDelayed(new Runnable() {
             public void run() {
                 launchMainActivity(intent);
@@ -28,6 +29,7 @@ public class WelcomeScreenActivity extends AppCompatActivity {
         for(int i = 0; i<3; i++){
             pulse(i*1000, logo);
         }
+        //If the users touch the screen before 3 sec, then launch HomeActivity
         View view = (View) findViewById(R.id.my_view);
         view.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -44,7 +46,7 @@ public class WelcomeScreenActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
-
+    //pulse : each 5 msec alpha decrease of 1% until 0.5sec, then each 5msec alpha increase of 1%
     private void pulse(int delay, ImageView logo){
         for(int i = 0; i<=1000; i+=5) {
             float opacity;
