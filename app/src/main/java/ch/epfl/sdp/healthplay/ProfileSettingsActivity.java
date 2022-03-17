ba035ad26c68c587fba870f5e6c316fdb286f217
ozan-guven <ozan.guven@epfl.ch>
2022-03-17 14:00:20 +0100

    Create user modification settings layout

---
+++ app/src/main/java/ch/epfl/sdp/healthplay/ProfileSettingsActivity.java
@@ -0,0 +1,14 @@
+package ch.epfl.sdp.healthplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_settings);
+    }
+}
