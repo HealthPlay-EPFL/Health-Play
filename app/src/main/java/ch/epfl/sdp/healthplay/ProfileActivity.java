2d6f1e03906c1d3d65c1f15cf1f513760db8a623
emonnin-epfl <etienne.monnin@epfl.ch>
2022-03-18 09:40:28 +0100

    add interactions to the profile interface

--- app/src/main/java/ch/epfl/sdp/healthplay/ProfileActivity.java
+++ app/src/main/java/ch/epfl/sdp/healthplay/ProfileActivity.java
@@ -17,6 +17,13 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 import ch.epfl.sdp.healthplay.database.User;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -24,60 +31,209 @@ public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        //FirebaseAuth.getInstance().signInWithEmailAndPassword("a@admin.ch","123456");
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null) {
+            User.writeNewUser(user.getUid(), "ayo", 0, 0);
+            initUsername(user.getUid());
+            initBirthday(user.getUid());
+            initStats(user.getUid());
+            initName(user.getUid());
+        }
+    }
 
-      // init_Display();
+    public void initBirthday(String userId) {
+        TextView TextViewBirthday = findViewById(R.id.profileBirthday);
+
+            User.readField(userId, User.BIRTHDAY, (task -> {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                } else {
+                    TextViewBirthday.setText(String.valueOf(task.getResult().getValue()));
+                }
+            }));
+
+            User.mDatabase.child(User.USERS).child(userId).child(User.BIRTHDAY).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String birthday = dataSnapshot.getValue(String.class);
+                    String[] parts = birthday.split("-");
+                    birthday = parts[2] + "/" + parts[1] +"/" + parts[0];
+                    TextViewBirthday.setText(birthday);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Log.w("firebase", "Error:onCancelled", databaseError.toException());
+                }
+            });
+        }
+
+
+
+
+    public void initName(String userId) {
+        TextView TextViewName = findViewById(R.id.profileName);
+
+
+            User.readField(userId, User.NAME, (task -> {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                } else {
+                    User.readField(userId, User.SURNAME, (t -> {
+                        if (!t.isSuccessful()) {
+                            Log.e("firebase", "Error getting data", t.getException());
+                        } else {
+                            String name = task.getResult().getValue() + " " + t.getResult().getValue();
+                            TextViewName.setText(name);
+                        }
+                    }));
+                }
+            }));
+
+            User.mDatabase.child(User.USERS).child(userId).child(User.NAME).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    User.readField(userId, User.SURNAME, (task -> {
+                        if (!task.isSuccessful()) {
+                            Log.e("firebase", "Error getting data", task.getException());
+                        } else {
+                            String name = dataSnapshot.getValue(String.class) + " " + task.getResult().getValue(String.class);
+                            TextViewName.setText(name);
+                        }
+                    }));
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Log.w("firebase", "Error:onCancelled", databaseError.toException());
+                }
+            });
+
+            User.mDatabase.child(User.USERS).child(userId).child(User.SURNAME).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                    User.readField(userId, User.NAME, (task -> {
+                        if (!task.isSuccessful()) {
+                            Log.e("firebase", "Error getting data", task.getException());
+                        } else {
+                            String name = task.getResult().getValue(String.class) + " " + dataSnapshot.getValue(String.class);
+                            TextViewName.setText(name);
+                        }
+                    }));
+
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Log.w("firebase", "Error:onCancelled", databaseError.toException());
+                }
+            });
+
+
 
     }
 
-    public void incrCalories(View view) {
-        Intent intent = getIntent();
-        String id = intent.getStringExtra(ProfileSetupActivity.EXTRA_ID);
-        User.mDatabase.child("users").child(id).child("calorieCounter").get().addOnCompleteListener(task -> {
-            if (!task.isSuccessful()) {
-                Log.e("firebase", "Error getting data", task.getException());
-            }
-            else {
-                int counter = Integer.parseInt(String.valueOf(task.getResult().getValue()));
-                writeCalorie(id,counter + 1);
-            }
-        });
-
-    }
-
-    public void initCaloriCounter(String id) {
-
-        TextView TextView2 = findViewById(R.id.CalorieCounter);
-
-        User.mDatabase.child("users").child(id).child("calorieCounter").get().addOnCompleteListener(task -> {
-            if (!task.isSuccessful()) {
-                Log.e("firebase", "Error getting data", task.getException());
-            }
-            else {
-                int calories = Integer.parseInt(String.valueOf(task.getResult().getValue()));
-                String showCalories = calories + " calories!";
-                TextView2.setText(showCalories);
-            }
-        });
-    }
-
-    public void init_Display() {
-
-
-
+    private void initUsername(String userId) {
         TextView TextViewUsername = findViewById(R.id.profileUsername);
+
+            User.readField(userId, User.USERNAME, (task -> {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                } else {
+                    TextViewUsername.setText(String.valueOf(task.getResult().getValue()));
+                }
+            }));
+
+            User.mDatabase.child(User.USERS).child(userId).child(User.USERNAME).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String username = dataSnapshot.getValue(String.class);
+                    TextViewUsername.setText(username);
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Log.w("firebase", "Error:onCancelled", databaseError.toException());
+                }
+            });
+
+
+
+    }
+
+    public void initStats(String userId) {
+
+            User.getStats(userId,(task -> {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                } else {
+                    Map<String, Map<String, Number>> map = (Map<String, Map<String, Number>>)task.getResult().getValue();
+                    updateStats(map, userId);
+                }
+            }));
+
+            User.mDatabase.child(User.USERS).child(userId).child(User.STATS).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    @SuppressWarnings("unchecked")
+                    Map<String, Map<String, Number>> stats = (Map<String, Map<String, Number>>)dataSnapshot.getValue();
+                    if(stats != null) {
+                        updateStats(stats,userId);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Log.w("firebase", "Error:onCancelled", databaseError.toException());
+                }
+            });
+
+
+    }
+
+
+    public void updateStats(Map<String, Map<String, Number>> map, String userId) {
         TextView TextViewStatsButton = findViewById(R.id.statsButton);
         TextView TextViewWeight = findViewById(R.id.profileWeight);
-        TextView TextViewBirthday = findViewById(R.id.profileBirthday);
         TextView TextViewHealthPoint = findViewById(R.id.profileHealthPoint);
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            //TODO ADD ACTIVITY
-        }
-       
+        if (map!=null && map.containsKey(User.getTodayDate())) {
+            Map<String, Number> todayStats = map.get(User.getTodayDate());
 
+            if (todayStats != null){
+                if(todayStats.containsKey(User.CALORIE_COUNTER) &&
+                  (todayStats.get(User.CALORIE_COUNTER)) != null) {
+                    String todayCalories = todayStats.get(User.CALORIE_COUNTER) + "\ndaily calories";
+                    TextViewStatsButton.setText(todayCalories);
+                }
+                else {
+                    TextViewStatsButton.setText("0\ndaily calories");
+                }
+                if(todayStats.containsKey(User.HEALTH_POINT) &&
+                  (todayStats.get(User.HEALTH_POINT)) != null) {
+                    TextViewHealthPoint.setText(String.valueOf(todayStats.get(User.HEALTH_POINT)));
+                }
+                else {
+                    TextViewWeight.setText("0");
+                }
+                if(todayStats.containsKey(User.WEIGHT) &&
+                  (todayStats.get(User.WEIGHT)) != null) {
+                    TextViewWeight.setText(String.valueOf(todayStats.get(User.WEIGHT)));
+                }
+                else {
+                    TextViewHealthPoint.setText("0");
+                }
+            }
+
+         }
+        else {
+            TextViewStatsButton.setText("0\ndaily calories");
+            TextViewWeight.setText("0");
+            TextViewHealthPoint.setText("0");
+        }
 
 
     }
+
 }
--- app/src/main/java/ch/epfl/sdp/healthplay/database/User.java
+++ app/src/main/java/ch/epfl/sdp/healthplay/database/User.java
@@ -2,18 +2,26 @@ package ch.epfl.sdp.healthplay.database;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -28,11 +36,15 @@ public final class User {
     public static final String AGE = "age";
     public static final String HEALTH_POINT = "healthPoint";
     public static final String WEIGHT = "weight";
+    public static final String LAST_CURRENT_WEIGHT = "lastCurrentWeight";
+    public static final String NAME = "name";
+    public static final String SURNAME = "surname";
+    public static final String BIRTHDAY = "birthday";
 
     public static DatabaseReference mDatabase = FirebaseDatabase.getInstance(DATABASE_URL).getReference();
 
-    private final static String STATS = "stats";
-    private static final String USERS = "users";
+    public final static String STATS = "stats";
+    public static final String USERS = "users";
 
     private String username;
     private String name;
@@ -41,6 +53,7 @@ public final class User {
     // A yyyy-MM-dd formatted date
     private String birthday;
     private int age;
+    private String lastCurrentWeight;
 
     // Format used to format date when adding stats
     private static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
@@ -55,24 +68,79 @@ public final class User {
         this.email = email;
         this.birthday = birthday;
         this.age = age;
+        this.lastCurrentWeight = "0";
     }
 
+    /**
+     * Creates a new user in the database
+     *
+     * @param userId   the unique identifier given to the user
+     * @param userName the username
+     * @param age      the age of the user
+     * @param weight   the weight of the user
+     */
     public static void writeNewUser(String userId, String userName, int age, int weight) {
-        User user = new User(userName, "empty name", "emtpy surname", "empty@email.com", "2000-01-01", age);
+        User user = new User(userName, "empty name", "empty surname", "empty@email.com", "2000-01-01", age);
         mDatabase.child(USERS).child(userId).setValue(user);
     }
 
     public static void writeUsername(String userId, String name) {
-        mDatabase.child(USERS).child(userId).child(USERNAME).setValue(name);
+        mDatabase
+                .child(USERS)
+                .child(userId)
+                .child(USERNAME)
+                .setValue(name);
     }
 
+    /**
+     * Writes the calorie counter for today and overwrites any
+     * values that were present before.
+     *
+     * @param userId         the user ID
+     * @param calorieCounter the number of calories
+     */
     public static void writeCalorie(String userId, int calorieCounter) {
         mDatabase.child(USERS)
                 .child(userId)
                 .child(STATS)
-                .child(format.format(new Date()))
+                .child(getTodayDate())
                 .child(CALORIE_COUNTER)
                 .setValue(calorieCounter);
+    }
+
+    /**
+     * Adds the given number of calories to the user's statistics. The
+     * difference with {@linkplain #writeCalorie(String, int)} is that
+     * this methods add to the current value contained for the day.
+     *
+     * @param userId   the user ID
+     * @param calories the number of calories to add
+     */
+    public static void addCalorie(String userId, int calories) {
+        getStats(userId, task -> {
+            if (!task.isSuccessful()) {
+                Log.e("ERROR", "EREREREROOORORO");
+            }
+            int toAdd = calories;
+            @SuppressWarnings("unchecked")
+            Map<String, Map<String, Number>> map = (Map<String, Map<String, Number>>) task.getResult().getValue();
+            // This bellow is to check the existence of the wanted calories
+            // for today's date
+            if (map != null && map.containsKey(getTodayDate())) {
+                Map<String, Number> calo = map.get(getTodayDate());
+                long currentCalories;
+                if (calo != null && calo.containsKey(CALORIE_COUNTER)) {
+                    currentCalories = Long.parseLong(String.valueOf(calo.get(CALORIE_COUNTER)));
+                    toAdd += currentCalories;
+                }
+            }
+            mDatabase.child(USERS)
+                    .child(userId)
+                    .child(STATS)
+                    .child(getTodayDate())
+                    .child(CALORIE_COUNTER)
+                    .setValue(toAdd);
+        });
     }
 
     public static void writeAge(String userId, int age) {
@@ -83,18 +151,55 @@ public final class User {
         mDatabase.child(USERS)
                 .child(userId)
                 .child(STATS)
-                .child(format.format(new Date()))
+                .child(getTodayDate())
                 .child(HEALTH_POINT)
                 .setValue(healthPoint);
     }
 
-    public static void writeWeight(String userId, int weight) {
+    public static void writeWeight(String userId, double weight) {
+        mDatabase.child(USERS)
+                .child(userId)
+                .child(LAST_CURRENT_WEIGHT)
+                .setValue(weight);
+
         mDatabase.child(USERS)
                 .child(userId)
                 .child(STATS)
-                .child(format.format(new Date()))
+                .child(getTodayDate())
                 .child(WEIGHT)
                 .setValue(weight);
+    }
+
+    public static void writeName(String userId, String name) {
+        mDatabase.child(USERS)
+                .child(userId)
+                .child(NAME)
+                .setValue(name);
+    }
+
+    public static void writeSurname(String userId, String surname) {
+        mDatabase.child(USERS)
+                .child(userId)
+                .child(SURNAME)
+                .setValue(surname);
+    }
+
+    public static void writeBirthday(String userId, String birthday) {
+        mDatabase.child(USERS)
+                .child(userId)
+                .child(SURNAME)
+                .setValue(birthday);
+        try {
+            Date birth = format.parse(birthday);
+            Date today = new Date();
+            Calendar calendar = Calendar.getInstance();
+            if (birth != null){
+                calendar.setTimeInMillis(today.getTime() - birth.getTime());
+                int age  = calendar.get(Calendar.YEAR);
+                writeAge(userId, age);
+            }
+        } catch (ParseException ignored) {
+        }
     }
 
     public static void deleteUser(String userId) {
@@ -125,6 +230,10 @@ public final class User {
         return age;
     }
 
+    public String getLastCurrentWeight() {
+        return lastCurrentWeight;
+    }
+
     /**
      * Given a userId, return the map containing each dates in the
      * yyyy-MM-dd format, each of which contains a map of available
@@ -146,50 +255,50 @@ public final class User {
      * {@code {"2022-03-04"={"caloriesCount"=123, "healthPoints"=123, "weight"=123}, ...}}
      *
      * @param userId the id of the user
-     * @return A map representing multiple maps of the given example.
-     * If a user has no stats, this method will return an empty map.
      */
-    public static Map<String, Map<String, String>> getStats(String userId) {
-        Map<String, Map<String, String>> map = new HashMap<>();
-        Task<DataSnapshot> t = mDatabase.child(USERS)
+    public static void getStats(String userId, OnCompleteListener<DataSnapshot> onCompleteListener) {
+        mDatabase.child(USERS)
                 .child(userId)
                 .child(STATS)
                 .get()
-                .addOnCompleteListener(task -> map.putAll(listenerTask(task)));
-
-        try {
-            // Wait for the listener to complete to get the result
-            Tasks.await(t, 10, TimeUnit.SECONDS);
-        } catch (ExecutionException | InterruptedException | TimeoutException e) {
-            e.printStackTrace();
-        }
-
-        return map;
+                .addOnCompleteListener(onCompleteListener);
     }
 
-
-    // Helper function used inside the addOnCompleteListener of getStats method
-    private static Map<String, Map<String, String>> listenerTask(Task<DataSnapshot> task) {
-        if (!task.isSuccessful()) {
-            Log.e("firebase", "Error getting data", task.getException());
-        }
-
-        try {
-            // This SuppressWarnings is used because the data taken from
-            // the database should be a JSON that is either empty or not
-            @SuppressWarnings("unchecked")
-            Map<String, Map<String, String>> result =
-                    (Map<String, Map<String, String>>) task.getResult().getValue();
-            if (result != null) {
-                return result;
-            }
-        } catch (ClassCastException ignored) {
-            // If a cast exception is thrown, it is ignored and
-            // the function will return an empty map
-        }
-        return new HashMap<>();
+    public static void readField(String userId, String field, OnCompleteListener<DataSnapshot> listener) {
+        User.mDatabase.child("users").child(userId).child(field).get().addOnCompleteListener(listener);
     }
 
+    /**
+     * Gets the current date in the yyyy-MM-dd format
+     *
+     * @return the current date in the yyyy-MM-dd format
+     */
+    @NonNull
+    public static String getTodayDate() {
+        return format.format(new Date());
+    }
 
-
+    /**
+     * Gets the last entered weight for the given user if present
+     *
+     * @param userId the user id
+     * @return the string of the last entered weight if the user ever entered one,
+     * the empty string otherwise
+     */
+    public static String getLastEnteredWeight(String userId) {
+        StringBuilder weight = new StringBuilder();
+        mDatabase
+                .child(USERS)
+                .child(userId)
+                .child(LAST_CURRENT_WEIGHT)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (!task.isSuccessful()) {
+                        Log.e("firebase", "Error getting data", task.getException());
+                    } else {
+                        weight.append(task.getResult());
+                    }
+                });
+        return weight.toString();
+    }
 }
