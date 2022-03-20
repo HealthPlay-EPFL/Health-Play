b73419fcb11a5bbecca7621e21cdfb08a1868b15
Aco <albin.vernhes@epfl.ch>
2022-03-20 14:23:46 +0100

    bug fix settings view

--- app/src/main/java/ch/epfl/sdp/healthplay/auth/SignedInActivity.java
+++ app/src/main/java/ch/epfl/sdp/healthplay/auth/SignedInActivity.java
@@ -85,7 +85,6 @@ public class SignedInActivity extends AppCompatActivity {
             populateIdpToken(response);
 
             mBinding.deleteAccount.setOnClickListener(view -> deleteAccountClicked());
-
             mBinding.signOut.setOnClickListener(view -> signOut());
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
