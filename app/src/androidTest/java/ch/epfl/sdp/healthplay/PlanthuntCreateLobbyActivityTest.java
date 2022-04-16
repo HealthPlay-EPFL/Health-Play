ff6bc2a624920efc429d2f264b270145e33aa24a
Zephyr75 <antoine.familleroger@gmail.com>
2022-04-16 12:15:13 +0200

    Fix test

--- app/src/androidTest/java/ch/epfl/sdp/healthplay/PlanthuntCreateLobbyActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/healthplay/PlanthuntCreateLobbyActivityTest.java
@@ -1,15 +1,24 @@
 package ch.epfl.sdp.healthplay;
 
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.*;
 
+import android.view.View;
+
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 
