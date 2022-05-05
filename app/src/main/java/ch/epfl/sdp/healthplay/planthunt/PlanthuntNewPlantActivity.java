65af170cdff86624f3311c3b39c6fa481211040c
Zephyr75 <antoine.familleroger@gmail.com>
2022-05-05 12:05:20 +0200

    First point system

--- app/src/main/java/ch/epfl/sdp/healthplay/planthunt/PlanthuntNewPlantActivity.java
+++ app/src/main/java/ch/epfl/sdp/healthplay/planthunt/PlanthuntNewPlantActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,7 +12,10 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import java.util.Objects;
+
 import ch.epfl.sdp.healthplay.R;
+import ch.epfl.sdp.healthplay.database.Database;
 
 public class PlanthuntNewPlantActivity extends AppCompatActivity {
 
@@ -28,6 +32,17 @@ public class PlanthuntNewPlantActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String lobbyName = intent.getStringExtra(PlanthuntCreateJoinLobbyActivity.LOBBY_NAME);
         String currentUsername = intent.getStringExtra(PlanthuntCreateJoinLobbyActivity.USERNAME);
+
+        //Initialize database reference
+        Database db = new Database();
+
+        db.getLobbyPlayerScore(lobbyName, currentUsername, task -> {
+            if (!task.isSuccessful()) {
+
+                Log.e("ERROR", "An error happened");
+            }
+            db.updateLobbyPlayerScore(lobbyName, currentUsername, Math.toIntExact((long) task.getResult().getValue()) + 50);
+        });
 
         imageUrl = getIntent().getStringExtra(PlanthuntLobbyActivity.URL);
         System.out.println(imageUrl);
