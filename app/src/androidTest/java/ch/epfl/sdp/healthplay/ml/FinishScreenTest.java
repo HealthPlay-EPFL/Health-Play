20258ceec28d5561d3e3a52752651ae352bf91a8
Aco <albin.vernhes@epfl.ch>
2022-05-12 12:11:17 +0200

    cirus fix

--- app/src/androidTest/java/ch/epfl/sdp/healthplay/ml/FinishScreenTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/healthplay/ml/FinishScreenTest.java
@@ -1,20 +1,19 @@
 package ch.epfl.sdp.healthplay.ml;
 
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-
+import android.view.View;
 import android.content.Intent;
 import android.view.View;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.Espresso;
-import androidx.test.espresso.UiController;
-import androidx.test.espresso.ViewAction;
-import androidx.test.espresso.assertion.ViewAssertions;
-import androidx.test.espresso.matcher.ViewMatchers;
+
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -37,7 +36,7 @@ public class FinishScreenTest {
     @Test
     public void restartTest() {
         ActivityScenario.launch(FinishScreen.class);
-        Espresso.onView(withId(R.id.restart)).perform(click());
+        Espresso.onView(withId(R.id.restart)).perform(closeSoftKeyboard()).perform(click());
         Espresso.onView(withId(R.id.friends)).check(matches(isDisplayed()));
 
     }
@@ -45,8 +44,9 @@ public class FinishScreenTest {
     @Test
     public void quitTest() {
         ActivityScenario.launch(FinishScreen.class);
+
         Espresso.onView(withId(R.id.quit)).perform(click());
-        Espresso.onView(withId(R.id.bottomNavigationView)).check(matches(isDisplayed()));
+        Espresso.onView(withId(R.id.bottomNavigationView)).perform(closeSoftKeyboard()).check(matches(isDisplayed()));
 
     }
 }
