cfcd06d2d388c60cd6f74862937da67de124cc10
Zephyr75 <antoine.familleroger@gmail.com>
2022-05-13 00:42:24 +0200

    Added collection test

--- app/src/androidTest/java/ch/epfl/sdp/healthplay/planthunt/PlanthuntCollectionActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/healthplay/planthunt/PlanthuntCollectionActivityTest.java
@@ -1,16 +1,24 @@
 package ch.epfl.sdp.healthplay.planthunt;
 
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.not;
 
+import android.content.Intent;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.Espresso;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
+import ch.epfl.sdp.healthplay.R;
 
 import ch.epfl.sdp.healthplay.planthunt.PlanthuntResultActivity;
 
@@ -47,7 +55,7 @@ public class PlanthuntCollectionActivityTest {
         assertEquals(actual, expected);
     }*/
 
-    /*
+
     @Test
     public void itemIsCorrectlyDisplayed(){
         Intent intent = new Intent(ApplicationProvider.getApplicationContext(), PlanthuntCollectionItemActivity.class);
@@ -55,10 +63,8 @@ public class PlanthuntCollectionActivityTest {
         intent.putExtra("date", "testDate");
         intent.putExtra("image", "testImage");
 
-        //Espresso.onView(withId(R.id.buttonCapture)).check(matches(not(isDisplayed())));
-        //Espresso.onView(withId(R.id.buttonCapture)).check();
-
+        Espresso.onView(withId(R.id.planthuntMainLeave)).check(matches(not(isDisplayed())));
     }
-    */
+
 
 }
