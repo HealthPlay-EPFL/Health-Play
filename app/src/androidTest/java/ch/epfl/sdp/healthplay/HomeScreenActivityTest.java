7306ccf94798acce4d58c767caa81e6efc35c683
Tetardfurieux <56368615+Tetardfurieux@users.noreply.github.com>
2022-05-26 16:56:34 +0200

    fix conflict pb

--- app/src/androidTest/java/ch/epfl/sdp/healthplay/HomeScreenActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/healthplay/HomeScreenActivityTest.java
@@ -1,16 +1,15 @@
 package ch.epfl.sdp.healthplay;
 
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentContainerView;
-import androidx.fragment.app.testing.FragmentScenario;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,16 +19,11 @@ import com.google.firebase.database.DataSnapshot;
 
 import org.hamcrest.Matchers;
 import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import ch.epfl.sdp.healthplay.database.Database;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import ch.epfl.sdp.healthplay.database.DataCache;
+import ch.epfl.sdp.healthplay.database.Database;
 
 @RunWith(AndroidJUnit4.class)
 public class HomeScreenActivityTest {
@@ -48,7 +42,7 @@ public class HomeScreenActivityTest {
     }
 
     @Test
-    public void isOnline(){
+    public void isOnline() {
         Database database = new Database();
         database.readField(FirebaseAuth.getInstance().getCurrentUser().getUid(), "onlineStatus", new OnCompleteListener<DataSnapshot>() {
             @Override
@@ -56,6 +50,7 @@ public class HomeScreenActivityTest {
                 assertThat(task.getResult().getValue(String.class), Matchers.equalTo("online"));
             }
         });
+    }
       
     @Test
     public void activityInitialization() {
